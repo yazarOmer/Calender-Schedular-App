6e4d48f8e579e48ab98ca10123143516c3deeb9a
ömer <omeryazar0808@gmail.com>
2023-06-13 21:12:13 +0300

    add login functioanility

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch} from 'react-redux'
+import {logout, reset} from '../features/auth/authSlice'
 
 const Home = () => {
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
+
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/login')
+  }
+
   return (
-    <div className='text-black'>Home</div>
+    <div className=''>
+      <header className='flex justify-between items-center px-5 w-full h-20 shadow-sm bg-white'>
+        <h1 className='text-2xl font-bold'>CalenderApp</h1>
+        
+        <ul className='flex gap-2'>
+          {user ? (<>
+            <li>
+              <button onClick={onLogout} className='px-5 py-3 font-bold'>Logout</button>
+            </li>
+          </>) : (<>
+            <li>
+              <Link to='/login' className='px-5 py-3 font-bold'>Login</Link>
+            </li>
+            <li>
+              <Link to='/register' className='px-5 py-3 font-bold'>Register</Link>
+            </li>
+          </>)}
+          
+        </ul>
+      </header>
+    </div >
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,8 @@
 import React, { useState, useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
 
 const Login = () => {
 
@@ -9,6 +13,25 @@ const Login = () => {
 
   const {email, password} = formData
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+
+    if(isError) {
+        toast.error(message)
+    }
+
+    if(isSuccess || user) {
+        navigate('/')
+    }
+
+    dispatch(reset())
+
+}, [user, isError, isSuccess, message, navigate, dispatch])
+
   const onChangeHandler = (e) => {
       setFormData((prevState) => ({
           ...prevState,
@@ -18,6 +41,13 @@ const Login = () => {
 
   const onSubmitHandler = (e) => {
       e.preventDefault()
+
+      const userData = {
+        email,
+        password
+      }
+
+      dispatch(login(userData))
   }
 
   return (
